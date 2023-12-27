b6422bc09a26540d7e19782bfa8354a8ad8d0563
raz610 <151939455+raz610@users.noreply.github.com>
2023-12-27 16:03:46 +0200

    Add files via upload

---
+++ Wallpaper.ps1
@@ -0,0 +1,16 @@
+﻿$RegPath = "HKCU:\Control Panel\Desktop"
+$ImageURL = 'https://www.tufin.com/wp-content/uploads/2023/07/tufin-image.jpg'
+$ImageLocation = "C:\Temp\Wallpaper"
+
+#Donwload image if not exsist
+if (-not (Test-Path $ImageLocation -PathType Leaf)) 
+  {
+   # Create image directory
+md $ImageLocation -ErrorAction SilentlyContinue
+
+# Download image files
+Start-BitsTransfer -Source $ImageURL -Destination $ImageLocation
+}
+
+# Background Wallpaper Registry Keys
+Set-ItemProperty -Path $RegPath -Name WallPaper -Value C:\Temp\Wallpaper\tufin-image.jpg -Force
