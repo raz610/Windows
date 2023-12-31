4a587aaa1319f237f89d1f96caa053cd52f65624
raz610 <151939455+raz610@users.noreply.github.com>
2023-12-31 11:30:25 +0200

    Update Wallpaper.ps1

--- Wallpaper.ps1
+++ Wallpaper.ps1
@@ -1,6 +1,7 @@
-﻿$RegPath = "HKCU:\Control Panel\Desktop"
+$RegPath = "HKCU:\Control Panel\Desktop"
 $ImageURL = 'https://www.tufin.com/wp-content/uploads/2023/07/tufin-image.jpg'
 $ImageLocation = "C:\Temp\Wallpaper"
+$ScriptURL = "https://raw.githubusercontent.com/raz610/Windows/main/Wallpaper.ps1"
 
 #Donwload image if not exsist
 if (-not (Test-Path $ImageLocation -PathType Leaf)) 
@@ -10,7 +11,18 @@ md $ImageLocation -ErrorAction SilentlyContinue
 
 # Download image files
 Start-BitsTransfer -Source $ImageURL -Destination $ImageLocation
+Start-BitsTransfer -Source $ScriptURL -Destination $ImageLocation
 }
 
 # Background Wallpaper Registry Keys
 Set-ItemProperty -Path $RegPath -Name WallPaper -Value C:\Temp\Wallpaper\tufin-image.jpg -Force
+
+#task_scheduler
+$scriptPath = "C:\Temp\Wallpaper\Wallpaper.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File $scriptPath"
+$trigger = New-ScheduledTaskTrigger -AtLogOn
+$taskName = "Wallpaper"  
+
+if (-not (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue)) {
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName
+}
